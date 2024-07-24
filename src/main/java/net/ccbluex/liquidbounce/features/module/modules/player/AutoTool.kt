53bfcd48d48dfac3bdf0ca25b9373713e4a902ca
LegiteriumZ <nhbinh2206@gmail.com>
2024-07-24 21:51:51 +0700

    update autotool

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/player/AutoTool.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/player/AutoTool.kt
@@ -7,28 +7,51 @@ package net.ccbluex.liquidbounce.features.module.modules.player
 
 import net.ccbluex.liquidbounce.event.ClickBlockEvent
 import net.ccbluex.liquidbounce.event.EventTarget
+import net.ccbluex.liquidbounce.event.UpdateEvent
+import net.ccbluex.liquidbounce.features.module.ModuleInfo
 import net.ccbluex.liquidbounce.features.module.Module
 import net.ccbluex.liquidbounce.features.module.ModuleCategory
-import net.ccbluex.liquidbounce.features.module.ModuleInfo
+import net.ccbluex.liquidbounce.utils.render.FakeItemRender
+import net.ccbluex.liquidbounce.features.value.BoolValue
 import net.minecraft.util.BlockPos
 
 @ModuleInfo(name = "AutoTool", category = ModuleCategory.PLAYER)
-class AutoTool : Module() {
+object AutoTool : Module() {
+
+    private val fakeItem = BoolValue("FakeItem", false)
+    private val switchBack = BoolValue("SwitchBack", false)
+    private val onlySneaking = BoolValue("OnlySneaking", false)
 
     @EventTarget
     fun onClick(event: ClickBlockEvent) {
         switchSlot(event.clickedBlock ?: return)
     }
 
+    var formerSlot = -1;
+
+    @EventTarget
+    fun onUpdate(event: UpdateEvent) {
+        // set fakeItem to null if mouse is not pressed
+        if (!mc.gameSettings.keyBindAttack.isKeyDown) {
+            if (switchBack.value && formerSlot != -1) {
+                mc.thePlayer.inventory.currentItem = formerSlot
+                formerSlot = -1
+            }
+            FakeItemRender.fakeItem = -1
+        }
+    }
+
     fun switchSlot(blockPos: BlockPos) {
         var bestSpeed = 1F
         var bestSlot = -1
 
-        val block = mc.theWorld.getBlockState(blockPos).block
+        val blockState = mc.theWorld.getBlockState(blockPos)
+
+        if (onlySneaking.value && !mc.thePlayer.isSneaking) return
 
         for (i in 0..8) {
             val item = mc.thePlayer.inventory.getStackInSlot(i) ?: continue
-            val speed = item.getStrVsBlock(block)
+            val speed = item.getStrVsBlock(blockState.block)
 
             if (speed > bestSpeed) {
                 bestSpeed = speed
@@ -36,8 +59,16 @@ class AutoTool : Module() {
             }
         }
 
-        if (bestSlot != -1) {
+        if (bestSlot != -1 && mc.thePlayer.inventory.currentItem != bestSlot) {
+            if (!fakeItem.value && FakeItemRender.fakeItem == -1) {
+                FakeItemRender.fakeItem = mc.thePlayer.inventory.currentItem
+            }
+            if (formerSlot == -1) {
+                formerSlot = mc.thePlayer.inventory.currentItem
+            }
             mc.thePlayer.inventory.currentItem = bestSlot
         }
+
     }
+
 }
